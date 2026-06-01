62df0da6786112fc12fe47488a3455335962935e
Suzal-Bookle-dev <suzal368@gmail.com>
2026-06-01 20:26:14 +0530

    fix: security Stale jail directories

--- cmd/goboxd/main.go
+++ cmd/goboxd/main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"log"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/gofiber/fiber/v3"
 	"github.com/thesouldev/goboxd/internal/api"
 	"github.com/thesouldev/goboxd/internal/config"
 )
 
+func cleanStaleJails() {
+	files, _ := os.ReadDir(os.TempDir())
+	for _, f := range files {
+		if strings.HasPrefix(f.Name(), "gobox-") {
+			os.RemoveAll(filepath.Join(os.TempDir(), f.Name()))
+		}
+	}
+}
+
 func main() {
+	cleanStaleJails()
+
 	App := fiber.New()
 
 	cfg, err := config.Load("languages.yaml")
